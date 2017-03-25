5ac8a04bf0cd0ebb364e2f5d6e94e4d09f4721bf
yangrenmu <1163393838@qq.com>
2017-03-25 17:14:27 +0800

    http

--- nuomi/musicPlayer/js/musicList.js
+++ nuomi/musicPlayer/js/musicList.js
@@ -2,26 +2,26 @@ let musicList = [
 	{
 		"name": "好心分手",
 		"singer": "卢巧音，王力宏",
-		"pic": "https://musicdata.baidu.com/data2/pic/86380337/86380337.jpg@s_0,w_300",
-		"url": "https://yinyueshiting.baidu.com/data2/music/108951897/72811121490425261128.mp3?xcode=c8de55577b3c6fa5e9c5304b21fec782"
+		"pic": "http://musicdata.baidu.com/data2/pic/86380337/86380337.jpg@s_0,w_300",
+		"url": "http://yinyueshiting.baidu.com/data2/music/108951897/72811121490425261128.mp3?xcode=c8de55577b3c6fa5e9c5304b21fec782"
 	},
 	{
 		"name": "酷爱",
 		"singer": "张敬轩",
-		"pic": "https://pic.xiami.net/images/album/img75/1175/3772991464956720.jpg?x-oss-process=image/resize,limit_0,m_pad,w_185,h_185",
-		"url": "https://m10.music.126.net/20170325164957/d9d6e8af5422ac933c50c3424260254b/ymusic/3b65/e449/5dbf/bbc366c82628d65ffde64b431b972d14.mp3"
+		"pic": "http://pic.xiami.net/images/album/img75/1175/3772991464956720.jpg?x-oss-process=image/resize,limit_0,m_pad,w_185,h_185",
+		"url": "http://m10.music.126.net/20170325164957/d9d6e8af5422ac933c50c3424260254b/ymusic/3b65/e449/5dbf/bbc366c82628d65ffde64b431b972d14.mp3"
 	},
 	{
 		"name": "爱得起",
 		"singer": "钟嘉欣",
 		"pic": "https://pic.xiami.net/images/album/img89/43189/19102476491472628482.jpg?x-oss-process=image/resize,limit_0,m_pad,w_185,h_185",
-		"url": "https://m10.music.126.net/20170325165632/870bbdcfd6629274cd76911be40225b9/ymusic/756b/f2b1/a437/8e3866bd5134d4802b87838f1ad41421.mp3"
+		"url": "http://m10.music.126.net/20170325165632/870bbdcfd6629274cd76911be40225b9/ymusic/756b/f2b1/a437/8e3866bd5134d4802b87838f1ad41421.mp3"
 	},
 	{
 		"name": "快乐崇拜",
 		"singer": "潘玮柏，张韶涵",
-		"pic": "https://img1.doubanio.com/lpic/s3174128.jpg",
-		"url": "https://mr3.doubanio.com/c6cb592e3e6e257e1736ebfef5b0f015/1/fm/song/p291936_128k.mp4"
+		"pic": "http://img1.doubanio.com/lpic/s3174128.jpg",
+		"url": "http://mr3.doubanio.com/c6cb592e3e6e257e1736ebfef5b0f015/1/fm/song/p291936_128k.mp4"
 	}
 
 ]
