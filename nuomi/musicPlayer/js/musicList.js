a05af13c6706c5bec1b172a6dd77b7fd16ed4c10
yangrenmu <1163393838@qq.com>
2017-04-03 10:37:02 +0800

    音乐播放器

--- nuomi/musicPlayer/js/musicList.js
+++ nuomi/musicPlayer/js/musicList.js
@@ -8,35 +8,20 @@ let musicList = [
 	{
 		"name": "酷爱",
 		"singer": "张敬轩",
-<<<<<<< HEAD
 		"pic": "img/zhangjingxuan.jpg",
 		"url": "musics/kuai.mp3"
-=======
-		"pic": "http://pic.xiami.net/images/album/img75/1175/3772991464956720.jpg?x-oss-process=image/resize,limit_0,m_pad,w_185,h_185",
-		"url": "http://yinyueshiting.baidu.com/data2/music/134374387/3002571490342461128.mp3?xcode=3258613ce12bd4f137ff48ca31f559c2"
->>>>>>> 12248dd8b50b805f900fa95ba06eef5b036d3e03
 	},
 	{
 		"name": "爱得起",
 		"singer": "钟嘉欣",
-<<<<<<< HEAD
 		"pic": "img/zhongjiaxin.jpg",
 		"url": "musics/aideqi.mp3"
-=======
-		"pic": "https://pic.xiami.net/images/album/img89/43189/19102476491472628482.jpg?x-oss-process=image/resize,limit_0,m_pad,w_185,h_185",
-		"url": "http://om5.alicdn.com/189/43189/431214/1770095119_59945170_l.mp3?auth_key=6af6d315b629cd6d0037ec2ab2916794-1491015600-0-null"
->>>>>>> 12248dd8b50b805f900fa95ba06eef5b036d3e03
 	},
 	{
 		"name": "快乐崇拜",
 		"singer": "潘玮柏，张韶涵",
-<<<<<<< HEAD
 		"pic": "img/panweibo.jpg",
 		"url": "musics/kuailechongbai.mp3"
-=======
-		"pic": "http://img1.doubanio.com/lpic/s3174128.jpg",
-		"url": "http://mr3.doubanio.com/c6cb592e3e6e257e1736ebfef5b0f015/1/fm/song/p291936_128k.mp4"
->>>>>>> 12248dd8b50b805f900fa95ba06eef5b036d3e03
 	}
 
 ]
